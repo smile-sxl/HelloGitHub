845c8452c15ae1a0bee7305fd9621998d933faac
smile-sxl <442053078@qq.com>
2017-04-12 15:30:40 +0800

    添加了一个按钮的点击事件并且取消了一个TextView

--- app/src/main/java/sany/com/hellogithub/MainActivity.java
+++ app/src/main/java/sany/com/hellogithub/MainActivity.java
@@ -2,17 +2,32 @@ package sany.com.hellogithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn:
+                Toast.makeText(this, "添加了点击事件", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
